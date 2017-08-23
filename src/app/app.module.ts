847a6d7eee4f2d4bdc88784a8c3c1eaf8a6f094b
Nikita Starostin <n.starostin@itransition.com>
2017-08-23 23:29:46 +0300

    initial markdown

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { MathComponent } from './math/math.component';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MathComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    RouterModule.forRoot([{path: '', component: MathComponent}])
   ],
   providers: [],
   bootstrap: [AppComponent]
